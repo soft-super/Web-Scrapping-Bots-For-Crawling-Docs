340a0ee454ed47472806c4578199ccc3081c6df5
Tosin K <komolafetosin@gmail.com>
2019-03-11 19:47:47 +0100

    intel_bot_sentenca_tj_mt

--- intel_bot_sentenca_rj_civel/setup.py
+++ intel_bot_sentenca_rj_civel/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name="intel_bot_sentenca_rj",
+setup(name="intel_bot_sentenca_rj_civel",
       version="1.0",
-      packages=["intel_bot_sentenca_rj"],
+      packages=["intel_bot_sentenca_rj_civel"],
       scripts=['bin/geckodriver'])
