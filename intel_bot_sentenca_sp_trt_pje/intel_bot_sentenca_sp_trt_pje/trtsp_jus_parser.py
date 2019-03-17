8fabb65c01c6191d1e9eec9d83af3724e2695d66
slarda <gianttiny123@gmail.com>
2019-03-17 01:41:10 -0400

    Update bot

--- intel_bot_sentenca_sp_trt_pje/intel_bot_sentenca_sp_trt_pje/trtsp_jus_parser.py
+++ intel_bot_sentenca_sp_trt_pje/intel_bot_sentenca_sp_trt_pje/trtsp_jus_parser.py
@@ -13,7 +13,9 @@ import requests
 
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 reload(sys)
 sys.setdefaultencoding('utf8')
